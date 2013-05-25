5007c0025e48969caad1b3016bf480c2bcfce2b3
Alberto Castro <alberto.castro@live.it>
2013-05-25 14:26:34 +0200

    Fixed AOF growth calculation overflow

--- src/redis.c
+++ src/redis.c
@@ -1061,14 +1061,13 @@ int serverCron(struct aeEventLoop *eventLoop, long long id, void *clientData) {
              server.aof_rewrite_perc &&
              server.aof_current_size > server.aof_rewrite_min_size)
          {
-            long long base = server.aof_rewrite_base_size ?
-                            server.aof_rewrite_base_size : 1;
-            long long growth = (server.aof_current_size*100/base) - 100;
-            if (growth >= server.aof_rewrite_perc) {
-                redisLog(REDIS_NOTICE,"Starting automatic rewriting of AOF on %lld%% growth",growth);
-                rewriteAppendOnlyFileBackground();
-            }
-         }
+			 off_t base = server.aof_rewrite_base_size ? server.aof_rewrite_base_size : 1;
+			 float growth = (1.0 * (server.aof_current_size - base)) / base * 100;
+			 if (growth >= server.aof_rewrite_perc) {
+				 redisLog(REDIS_NOTICE,"Starting automatic rewriting of AOF on %.2f%% growth", growth);
+				 rewriteAppendOnlyFileBackground();
+			 }
+		 }
     }
 
 
